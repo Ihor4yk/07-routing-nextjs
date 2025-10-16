1dc4f93fb79348a4b94a9f8cd8ca67a66be88106
Ihor4yk <igor4yk.lyashen4yk@gmail.com>
2025-10-16 19:57:44 +0300

    add NotePreview.client.tsx

---
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { fetchNoteById } from "@/lib/api";
+import Modal from "@/components/Modal/Modal";
+import { useParams } from "next/navigation";
+import css from "./NotePreview.module.css";
+
+export default function NotePreviewClient() {
+  const { id } = useParams<{ id: string }>();
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
+    refetchOnMount: false,
+  });
+
+  if (isLoading) return <p>Loading, please wait...</p>;
+  if (error || !data) return <p>Something went wrong.</p>;
+
+  return (
+    <Modal>
+      <div className={css.container}>
+        <h2 className={css.title}>{data.title}</h2>
+        <p className={css.content}>{data.content}</p>
+        <p className={css.date}>{new Date(data.createdAt).toLocaleString()}</p>
+      </div>
+    </Modal>
+  );
+}
--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -1,13 +1,15 @@
-type NotesLayoutProps = {
+import css from "./LayoutNotes.module.css";
+
+interface NotesLayoutProps {
   children: React.ReactNode;
   sidebar: React.ReactNode;
-};
+}
 
-export default function NotesLayout({ children, sidebar }: NotesLayoutProps) {
+export default function LayoutNotes({ children, sidebar }: NotesLayoutProps) {
   return (
-    <section>
-      <aside>{sidebar}</aside>
-      <div>{children}</div>
+    <section className={css.notesWrapper}>
+      <aside className={css.sidebar}>{sidebar}</aside>
+      <div className={css.container}>{children}</div>
     </section>
   );
 }
